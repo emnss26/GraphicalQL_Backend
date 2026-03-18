3711397ff8d07ad4ec35abc2e6621c0d4d5553d2
Enrique Meneses <109693762+emnss26@users.noreply.github.com>
2026-03-17 18:31:03 -0600

    Update v3 deploy

--- config.js
+++ config.js
@@ -85,7 +85,7 @@ const config = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD || "",
       database: process.env.DB_NAME,
-      port: Number.isFinite(dbPort) ? dbPort : 3306,
+      port: Number.isFinite(dbPort) ? dbPort : 3307,
     },
   },
 };
